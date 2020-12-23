22bb18d8bc92a6d3aa5912d65055be517a2f78cf
= <=>
2020-12-23 21:10:53 +0800

    sellerManager

--- be/model/store.py
+++ be/model/store.py
@@ -1,35 +1,63 @@
 import logging
 import os
-import psycopg2
-from sqlalchemy import create_engine
+import sqlite3 as sqlite
 
 
 class Store:
     database: str
-    user: str
-    password: str
-    host: str
-    port: str
 
-    def __init__(self):
-        self.engine = create_engine('postgresql://postgres:@localhost:5432/bookstore')
-    
-    def get_engine(self):
-        return self.engine
+    def __init__(self, db_path):
+        self.database = os.path.join(db_path, "be.db")
+        self.init_tables()
+
+    def init_tables(self):
+        try:
+            conn = self.get_db_conn()
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS user ("
+                "user_id TEXT PRIMARY KEY, password TEXT NOT NULL, "
+                "balance INTEGER NOT NULL, token TEXT, terminal TEXT);"
+            )
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS user_store("
+                "user_id TEXT, store_id, PRIMARY KEY(user_id, store_id));"
+            )
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS store( "
+                "store_id TEXT, book_id TEXT, book_info TEXT, stock_level INTEGER,"
+                " PRIMARY KEY(store_id, book_id))"
+            )
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS new_order( "
+                "order_id TEXT PRIMARY KEY, user_id TEXT, store_id TEXT)"
+            )
+
+            conn.execute(
+                "CREATE TABLE IF NOT EXISTS new_order_detail( "
+                "order_id TEXT, book_id TEXT, count INTEGER, price INTEGER,  "
+                "PRIMARY KEY(order_id, book_id))"
+            )
+
+            conn.commit()
+        except sqlite.Error as e:
+            logging.error(e)
+            conn.rollback()
+
+    def get_db_conn(self) -> sqlite.Connection:
+        return sqlite.connect(self.database)
 
 
 database_instance: Store = None
 
 
-def init_database(database="bookstore",
-                  user="root",
-                  password="123456",
-                  host="localhost",
-                  port="5432"):
+def init_database(db_path):
     global database_instance
-    database_instance = Store(database, user, password, host, port)
+    database_instance = Store(db_path)
 
 
-def get_engine():
+def get_db_conn():
     global database_instance
-    return database_instance.get_engine()
+    return database_instance.get_db_conn()
--- be/view/seller.py
+++ be/view/seller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import request
 from flask import jsonify
-from be.model import seller
+from be.model import sellerManager
 import json
 
 bp_seller = Blueprint("seller", __name__, url_prefix="/seller")
@@ -11,7 +11,7 @@ bp_seller = Blueprint("seller", __name__, url_prefix="/seller")
 def seller_create_store():
     user_id: str = request.json.get("user_id")
     store_id: str = request.json.get("store_id")
-    s = seller.Seller()
+    s = sellerManager.SellerManager()
     code, message = s.create_store(user_id, store_id)
     return jsonify({"message": message}), code
 
@@ -23,8 +23,11 @@ def seller_add_book():
     book_info: str = request.json.get("book_info")
     stock_level: str = request.json.get("stock_level", 0)
 
-    s = seller.Seller()
-    code, message = s.add_book(user_id, store_id, book_info.get("id"), json.dumps(book_info), stock_level)
+    s = sellerManager.SellerManager()
+
+    code, message = s.add_book(user_id, store_id, book_info)
+    if code == 200:
+        code, message = s.add_inventory(user_id, store_id, book_info, stock_level)
 
     return jsonify({"message": message}), code
 
@@ -36,7 +39,7 @@ def add_stock_level():
     book_id: str = request.json.get("book_id")
     add_num: str = request.json.get("add_stock_level", 0)
 
-    s = seller.Seller()
+    s = sellerManager.SellerManager()
     code, message = s.add_stock_level(user_id, store_id, book_id, add_num)
 
     return jsonify({"message": message}), code
